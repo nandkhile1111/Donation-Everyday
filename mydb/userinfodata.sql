fb2a94dab7c464454181ec06f5dd2a9d4ba51458
nandkhile1111 <92627743+nandkhile1111@users.noreply.github.com>
2021-10-17 16:28:04 +0530

    Comment1

---
+++ mydb/userinfodata.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 02, 2021 at 06:42 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `usersdata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfodata`
+--
+
+CREATE TABLE `userinfodata` (
+  `id` int(250) NOT NULL,
+  `user` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `mobile` varchar(250) NOT NULL,
+  `comment` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userinfodata`
+--
+
+INSERT INTO `userinfodata` (`id`, `user`, `email`, `mobile`, `comment`) VALUES
+(1, 'gnv ', 'hhdfcngv ', '64315216532', '     \r\n    tesdyrfgkhbljn.hgfnb '),
+(3, 'omkar', 'om@gmail.com ', '9955664488', '     how are you\r\n     ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userinfodata`
+--
+ALTER TABLE `userinfodata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userinfodata`
+--
+ALTER TABLE `userinfodata`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
